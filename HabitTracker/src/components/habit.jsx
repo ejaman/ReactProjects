ae6e7f871ff8b14efe7eca492bcbb223a33592e1
ejaman <leegm042@gmail.com>
2021-11-28 23:57:59 +0900

    add function to btn

--- HabitTracker/src/components/habit.jsx
+++ HabitTracker/src/components/habit.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 
 export default class Habit extends Component {
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit); //habits의 habit인자로 전달(?)
+  };
+
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleTrash = () => {
+    this.props.onTrash(this.props.habit);
+  };
+
   render() {
+    // console.log(this.props);
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-btn increase">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+
+        <button className="habit-btn increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-btn decrease">
+        <button className="habit-btn decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-btn trash">
+        <button className="habit-btn trash" onClick={this.handleTrash}>
           <i className="fas fa-trash-alt"></i>
         </button>
       </li>
