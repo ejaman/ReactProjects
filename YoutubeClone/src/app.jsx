852f259881e9dbe6cfdcf6d8e8e2234ae852976d
ejaman <leegm042@gmail.com>
2021-12-02 15:08:56 +0900

    remove .git

---
+++ YoutubeClone/src/app.jsx
@@ -0,0 +1,12 @@
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  return (
+    <>
+      <VideoList />
+    </>
+  );
+}
+
+export default App;
---
+++ YoutubeClone/src/components/video_list/video_list.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class VideoList extends Component {
+  render() {
+    return (
+      <>
+        <div>videos</div>
+        <ul>
+          <span> </span>
+        </ul>
+      </>
+    );
+  }
+}
